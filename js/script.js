77ba73b9e2ca86695614beae00046e0977940212
watataku <watataku131122@outlook.jp>
2022-01-14 17:03:23 +0900

    アルバム、Facebook作成

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 
-	$('.slider').slick({
-		autoplay: true,//自動的に動き出すか。初期値はfalse。
-		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-		slidesToShow: 2,//スライドを画面に2枚見せる
-		slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
-		prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-		nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-		dots: true,//下部ドットナビゲーションの表示
-	});
+$('.slider').slick({
+	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+	slidesToShow: 2,//スライドを画面に2枚見せる
+	slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+	dots: true,//下部ドットナビゲーションの表示
+});

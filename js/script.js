6108e05176f10afed75fd7728ac7242b573a6767
watataku <watataku131122@outlook.jp>
2022-01-17 01:35:44 +0900

    完成

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 $('.slider').slick({
-	autoplay: true,//自動的に動き出すか。初期値はfalse。
+	autoplay: false,//自動的に動き出すか。初期値はfalse。
 	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
 	slidesToShow: 2,//スライドを画面に2枚見せる
 	slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる

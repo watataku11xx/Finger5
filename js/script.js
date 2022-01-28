47fbe6af97103bee57c450612e66c9df0182f8cb
watataku <watataku131122@outlook.jp>
2022-01-28 12:22:27 +0900

    SP用‗レイアウト変更

--- js/script.js
+++ js/script.js
@@ -7,4 +7,14 @@ $('.slider').slick({
 	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
 	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
 	dots: true,//下部ドットナビゲーションの表示
+
+	responsive: [
+		{
+		breakpoint: 767,//モニターの横幅が767px以下の見せ方
+		settings: {
+			slidesToShow: 1,//スライドを画面に2枚見せる
+			slidesToScroll: 1,//1回のスクロールで2枚の写真を移動して見せる
+		}
+		}
+	]
 });

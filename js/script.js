269911909ebd40ae43186b561ce5a3f2f917686b
Watanabe Takuto <watataku131122@outlook.jp>
2022-02-07 01:01:51 +0900

    色々変更

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
 $('.slider').slick({
-	autoplay: false,//自動的に動き出すか。初期値はfalse。
-	infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-	slidesToShow: 2,//スライドを画面に2枚見せる
-	slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
-	prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-	nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-	dots: true,//下部ドットナビゲーションの表示
+	autoplay: false,
+	infinite: true,
+	slidesToShow: 2,
+	slidesToScroll: 2,
+	prevArrow: '<div class="slick-prev"></div>',
+	nextArrow: '<div class="slick-next"></div>',
+	dots: true,
 
 	responsive: [
 		{
-		breakpoint: 767,//モニターの横幅が767px以下の見せ方
+		breakpoint: 767,
 		settings: {
-			slidesToShow: 1,//スライドを画面に2枚見せる
-			slidesToScroll: 1,//1回のスクロールで2枚の写真を移動して見せる
+			slidesToShow: 1,
+			slidesToScroll: 1,
 		}
 		}
 	]
